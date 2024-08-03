21f95ae9d02c2d8d34f3038eee3c22021f070f14
LupinaIgor <igorlupina222@gmail.com>
2024-08-03 12:12:42 +0300

    start project

---
+++ homeTaskMyWebServer/homeTaskMyWebServer/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace homeTaskMyWebServer.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
